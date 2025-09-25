75dcd0740b4f899912a0cb4c0f211d9778e39ac7
mt <mei@pplab.jp>
2025-09-25 16:16:51 +0900

    Delete karuta directory

--- karuta/main/overall_max.m
+++
@@ -1,8 +0,0 @@
-
-function xmax=overall_max(x)
-   n=ndims(x);          
-   xmax=x;
-    for k=n:-1:1
-        xmax=max(xmax,[ ], k);
-    end
-end
--- karuta/main/overall_sum.m
+++
@@ -1,7 +0,0 @@
-function s=overall_sum(x)
-    n=ndims(x);          
-    s=x;
-    for k=n:-1:1
-        s=sum(s,k);
-    end
-end
